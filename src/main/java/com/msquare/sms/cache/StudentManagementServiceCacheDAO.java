ad00bd2deeeceb5fc6feb03906e3148793ceeb0d
msquare711 <mwardenmichl@gmail.com>
2016-05-26 21:17:51 -0700

    loaded student cache with data

--- src/main/java/com/msquare/sms/cache/StudentManagementServiceCacheDAO.java
+++ src/main/java/com/msquare/sms/cache/StudentManagementServiceCacheDAO.java
@@ -18,8 +18,45 @@ public class StudentManagementServiceCacheDAO {
 	public StudentManagementServiceCacheDAO() {
 		CacheManager.create();
 		cacheManager.addCache(new Cache(new CacheConfiguration("student", 1000).eternal(true)));
+		loadCacheWithDefaultStudents();
 	}
 	
+	private void loadCacheWithDefaultStudents() {
+		Cache studentCache = cacheManager.getCache("student");
+		Student one = new Student();
+		one.setId(1L);
+		one.setFirstName("Bobby");
+		one.setLastName("Smith");
+		one.setPhone("5551231212");
+		one.setState("AZ");
+		one.setStreet("123 Main St.");
+		one.setCity("Mesa");
+		one.setZipCode("85207");
+		studentCache.put(new Element(one.getId(), one));
+		
+		Student two = new Student();
+		two.setId(1L);
+		two.setFirstName("Cindy");
+		two.setLastName("Lou");
+		two.setPhone("5552334567");
+		two.setState("AZ");
+		two.setStreet("555 Ohnoe Ave.");
+		two.setCity("Tempe");
+		two.setZipCode("85205");
+		studentCache.put(new Element(two.getId(), two));
+		
+		Student three = new Student();
+		three.setId(1L);
+		three.setFirstName("Justin");
+		three.setLastName("Douche");
+		three.setPhone("5554567890");
+		three.setState("AZ");
+		three.setStreet("666 Douche Ln.");
+		three.setCity("Phoenix");
+		three.setZipCode("85203");
+		studentCache.put(new Element(three.getId(), three));
+	}
+
 	public Student queryStudent(Long id){
 		List<Student> students = queryAllStudents();
 		for(Student student : students){
